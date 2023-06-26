276885fcd9908bee3394906e6df883974a43a13a
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-06-26 01:29:10 -0500

    Fixed Chatbox Padding

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,20 +3,16 @@ import { Boundary } from '@/ui/boundary';
 import Sidebar from '@/ui/sidebar';
 import { Layout, Text} from '@vercel/examples-ui'
 import { Chat } from '../components/Chat'
-export default function Page(){
 
-    return (
-
-
-      <Boundary>
-        <section className="flex flex-col gap-3">
-            <Text variant="h2">AI Chat Bot:</Text>
-            <div className="lg:w-2/3">
-            <Chat />
-            </div>
-        </section>
-        </Boundary>
-    
-     
-);
+export default function Page() {
+  return (
+    <Boundary>
+      <section className="flex flex-col gap-3">
+        <Text variant="h2">AI Chat Bot:</Text>
+        <div className="w-full">
+          <Chat />
+        </div>
+      </section>
+    </Boundary>
+  );
 }
